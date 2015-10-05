09244fa4f9bee82ce1790881ff9417472a0b5c55
Hyacinthe Cartiaux <Hyacinthe.Cartiaux@uni.lu>
2015-10-05 18:43:06 +0200

    Cosmetic change (indentation)

--- manifests/value.pp
+++ manifests/value.pp
@@ -64,8 +64,8 @@ define sysctl::value(
   {
       # Delete the parameter
       exec { "sed -s -i '/^${parameter}[[:blank:]]*=.*$/d' ${sysctl::params::configfile}":
-          path    => '/usr/bin:/usr/sbin:/bin',
-          onlyif  => "grep '^${parameter}[[:blank:]]*=${value}$' ${sysctl::params::configfile}"
+          path   => '/usr/bin:/usr/sbin:/bin',
+          onlyif => "grep '^${parameter}[[:blank:]]*=${value}$' ${sysctl::params::configfile}"
       }
   }
 
